2a895709605a78c6bba491cd82b3f534ca00189e
Marcelo Luiz Pinheiro <marcelo.pinheiro@alunos.sc.senac.br>
2025-01-07 17:39:12 -0300

    resolve circular dependencies

--- uikit/build.gradle.kts
+++ uikit/build.gradle.kts
@@ -40,7 +40,4 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
-    implementation(project(":uikit"))
-    api(project(path = ":domain"))
 }
