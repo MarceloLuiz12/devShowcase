126bd381cc4c144b5d1a0f0ff9653ec2dd237bc3
Marcelo Luiz Pinheiro <marcelo.pinheiro@alunos.sc.senac.br>
2025-01-10 07:10:55 -0300

    create extension in domain

--- feature_showcase/src/main/java/com/example/feature_show_case/via_cep/AddressViewModel.kt
+++ feature_showcase/src/main/java/com/example/feature_show_case/via_cep/AddressViewModel.kt
@@ -1,15 +1,12 @@
 package com.example.feature_show_case.via_cep
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.example.domain.core.UseCase
+import com.example.domain.extensions.useCase
 import com.example.domain.usecase.GetAddressUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
-import org.koin.core.parameter.parametersOf
 
 class AddressViewModel : ViewModel(), KoinComponent {
     private val getAddressUseCase: GetAddressUseCase by useCase()
@@ -31,8 +28,3 @@ class AddressViewModel : ViewModel(), KoinComponent {
     }
 
 }
-
-inline fun <V, reified U> V.useCase() where U : UseCase<*, *>, V : ViewModel, V : KoinComponent =
-    inject<U> {
-        parametersOf(viewModelScope)
-    }
